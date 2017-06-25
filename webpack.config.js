7a7a70c3d4cfae6127b5dcdb84c41df11f30fa6c
manuelWeb <devries.webdesigner@gmail.com>
2017-06-25 13:47:18 +0200

    add ExtractTextPlugin to write css file prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // deviner le chemin de la racine en abs
 var path = require('path');
 var root = path.resolve(__dirname);
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var extractCSS = new ExtractTextPlugin('bundle.css')
 
 module.exports = {
   entry: {
@@ -21,8 +23,11 @@ module.exports = {
       },{
         test: /\.css$/,
         // attention plusieur loader > 'loaderS'
-        loaders: ['style-loader', 'css-loader']
+        loaders: extractCSS.extract(['css-loader'])
       }
     ]
-  }
+  },
+  plugins: [
+    extractCSS
+  ]
 }
